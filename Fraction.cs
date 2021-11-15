e80a6759fde36f4c292e302e299e6c7a1bc161bc
adushinova_ekaterina <adushinova2002@mail.ru>
2021-11-15 20:59:21 +0800

    Добавила класс Fraction, конструктор, метод вывода ответа в строковом представлении и автотесты

---
+++ Fraction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабораторная_работа__3
+{
+    public class Fraction
+    {
+        // поля класса
+        private int numerator; // числитель
+        private int denominator; // знаменатель
+
+        // конструктор
+        public Fraction(int numerator, int denominator)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+        // метод вывода ответа (строковое представление)
+        public override string ToString()
+        {
+            // числитель/знаменатель
+            return numerator.ToString() + "/" + denominator.ToString();
+        }
+    }
+}
